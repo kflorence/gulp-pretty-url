755a3ae96171fa022b2fd3c6c0959b08b4a2d0d3
Kyle Florence <kyle.florence@joinmosaic.com>
2014-12-06 23:46:27 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var path = require("path");
+var rename = require("gulp-rename");
+
+/**
+ * Generate pretty URLs for files piped through this method.
+ *
+ * @see `gulp-rename` for more information
+ *
+ * @param {Object} file Object containing file path information
+ * @return Modified file path object
+ */
+function prettyUrl(file) {
+  file.extname = ".html";
+
+  if (file.basename !== "index") {
+    file.dirname = path.join(file.dirname, file.basename);
+    file.basename = "index";
+  }
+
+  return file;
+}
+
+module.exports = rename(prettyUrl);
